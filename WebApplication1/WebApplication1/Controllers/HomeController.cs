8209e94aef6f488190f5ceba50f416856a5fa39b
wayne528 <b8753003@gmail.com>
2017-11-05 09:48:50 +0800

    All

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -25,6 +27,56 @@ namespace WebApplication1.Controllers
             //ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModela login)
+        {
+            if (ModelState.IsValid && LoginCheck(login))
+            {
+                // 輸入驗證成功，所有欄位有效
+                FormsAuthentication
+                    .RedirectFromLoginPage(login.Email, false);
+
+                return RedirectToAction("Index","Customers");
+            }
+
+            ModelState.AddModelError(
+                "Password", "您輸入的帳號或密碼錯誤，請重新輸入！");
+
+            foreach (var model in ModelState)
+            {
+                if (!ModelState.IsValidField(model.Key))
+                {
+                    object raw = model.Value.Value.RawValue;
+                    string str = model.Value.Value.AttemptedValue;
+
+                    foreach (var err in model.Value.Errors)
+                    {
+                        var errExc = err.Exception;
+                        var errMsg = err.ErrorMessage;
+                    }
+                }
+            }
+            return View();
+        }
+
+        private bool LoginCheck(LoginViewModela login)
+        {
+            if (login.Email != "test@example.com")
+            {
+                ModelState.AddModelError("Email", "帳號輸入錯誤");
+            }
+
+            return (
+                login.Email == "test@example.com" &&
+                login.Password == "123"
+                );
         }
     }
 }
---
+++ WebApplication1/WebApplication1/Models/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class LoginViewModela
+    {
+        [Required]
+        [Display(Name = "電子郵件")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密碼")]
+        public string Password { get; set; }
+
+        [Display(Name = "記住我?")]
+        public bool RememberMe { get; set; }
+
+    }
+}
